2d64136ac3ef5d9aa8c81ead1484a0ec39f7f25f
Jason Leveille <m@jasonleveille.com>
2012-12-14 10:57:24 -0500

    Adds python setup package file, refs #1346

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Fabtasktic",
+    version = "0.1",
+    install_requires = ['fabric'],
+    author = "NPP",
+    author_email = "it@nationalpriorities.org",
+    description = "Common Fabric Tasks for NPP",
+    url = "https://github.com/npp/fabtasktic"
+)
