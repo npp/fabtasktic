c940841b03514ca4ac196d287ebd76e04977ae0b
Jason Leveille <m@jasonleveille.com>
2012-12-18 12:55:11 -0500

    Adds pip support.

---
+++ fabtasktic/tasks/pip.py
@@ -0,0 +1,41 @@
+from fabric.api import task
+
+@task
+def freeze(packagename=None):
+    '''
+    fab staging pip.freeze
+    '''
+    sudo('%(bin_path)s/pip freeze' % env, env.env_user)
+
+@task
+def install(packagename=None):
+    '''
+    fab staging pip.install packagename[==X.X.X]
+    '''
+    if package is not None:
+        env.packagename = packagename
+        sudo('%(bin_path)s/pip install %(packagename)s' % env, env.env_user)
+    else:
+        raise ValueError("Missing package name")
+
+@task
+def uninstall(packagename=None):
+    '''
+    fab staging pip.uninstall packagename
+    '''
+    if package is not None:
+        env.packagename = packagename
+        sudo('%(bin_path)s/pip uninstall %(packagename)s' % env, env.env_user)
+    else:
+        raise ValueError("Missing package name")
+
+@task
+def upgrade(packagename=None):
+    '''
+    fab staging pip.upgrade packagename[==X.X.X]
+    '''
+    if package is not None:
+        env.packagename = packagename
+        sudo('%(bin_path)s/pip install -U %(packagename)s' % env, env.env_user)
+    else:
+        raise ValueError("Missing package name")
