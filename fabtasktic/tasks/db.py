ae57902764a051a91ba59a54c908c9d6adc88d9a
Jason Leveille <jason@leve.us>
2014-02-19 16:09:09 -0500

    Added support for database flag in db task

--- fabtasktic/tasks/db.py
+++ fabtasktic/tasks/db.py
@@ -4,13 +4,14 @@ from fabric.contrib.console import confirm
 
 
 @task
-def migrate(app=None):
+def migrate(app=None, database=None):
     """
-    fab staging db.migrate:appname
+    fab staging db.migrate:appname,database
     """
-    if app is not None:
-        if confirm("Are you sure you want migrate %s on %s" % (app, env.host)):
-            with cd(env.project_path):
-                sudo('%s && python manage.py migrate %s --settings=%s' % (env.activate, app, env.django_settings))
-    else:
+    if app is None:
         raise ValueError('Missing app to migrate')
+    if database is None:
+        raise ValueError('Missing database flag')
+    if confirm("Are you sure you want migrate %s on %s" % (app, env.host)):
+        with cd(env.project_path):
+            sudo('%s && python manage.py migrate %s --settings=%s --database=%s' % (env.activate, app, env.django_settings, database))
